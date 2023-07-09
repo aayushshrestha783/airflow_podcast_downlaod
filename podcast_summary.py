9e8744f76eb96cdab30c29a0864c2ff18c786f74
Aayush Shrestha <aayushstha54321@gmail.com>
2023-07-09 23:00:42 +0545

    database

--- podcast_summary.py
+++ podcast_summary.py
@@ -1,24 +1,44 @@
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+import xmltodict
+import requests
+import pendulum
+from airflow.decorators import dag, task
 import sys
 sys.path.append("/path/to/py_env/lib")
-from airflow.decorators import dag, task
-import pendulum
-import requests
-import xmltodict
+
 
 @dag(
-    dag_id = 'podcast_summary',
-    schedule_interval = '@daily',
-    start_date = pendulum.datetime(2023,7,8),
-    catchup = False
+    dag_id='podcast_summary',
+    schedule_interval='@daily',
+    start_date=pendulum.datetime(2023, 7, 8),
+    catchup=False
 )
-
 def podcast_summary():
+    create_database = SqliteOperator(
+        task_id='create_table_sqlite',
+        sql=r"""
+        CREATE TABLE IF NOT EXISTS episodes (
+            link TEXT PRIMARY KEY,
+            title TEXT,
+            filename TEXT,
+            published TEXT,
+            description TEXT,
+            transcript TEXT
+        );
+        """
+    )
+
     @task()
     def get_episode():
-        data = requests.get("https://www.marketplace.org/feed/podcast/marketplace/")
+        data = requests.get(
+            "https://www.marketplace.org/feed/podcast/marketplace/")
         feed = xmltodict.parse(data.text)
         episodes = feed["rss"]["channel"]["item"]
         print(f"Found {len(episodes)} episodes.")
-        return episodes                 
+        return episodes
+
     podcast_episodes = get_episode()
+    create_database.set_downstream(podcast_episodes)
+
+
 summary = podcast_summary()
